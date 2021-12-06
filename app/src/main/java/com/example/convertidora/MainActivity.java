b089d1c72a3b5dcc7803dfc5d4fa6c35f3c52d8f
vladi <vladimirallende7@gmail.com>
2021-12-06 08:10:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/convertidora/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.convertidora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void convertCurrency(View view) {
+
+        EditText cantidad_pesos = findViewById(R.id.cantidad_pesos);
+        String cambio = "0.00116";
+        TextView mostrar = findViewById(R.id.mostrar);
+
+        if (!cantidad_pesos.getText().toString().equals("")) {
+            Float euroValue = Float.parseFloat(cantidad_pesos.getText().toString()) * Float.parseFloat(cambio);
+            mostrar.setText(euroValue + "");
+        } else {
+            mostrar.setText(R.string.Sin_Valor);
+        }
+    }
+}
